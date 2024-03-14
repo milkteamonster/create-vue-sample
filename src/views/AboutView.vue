a106c0b7132e279076bf16159f7428577834fad6
milkteamonster <milkteamonster@gmail.com>
2024-03-15 00:54:06 +0800

    first add

---
+++ src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <h1>This is an about page 123</h1>
+    <p>{{ data }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {}
+    }
+  },
+  mounted() {
+    console.log(import.meta.env.VITE_TEXT)
+    const url = import.meta.env.VITE_PATH
+    this.$http.get(url).then((response) => {
+      console.log(response.data.results[0].location.country)
+      this.data = response.data.results[0]
+      // console.log(this.data)
+    })
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
